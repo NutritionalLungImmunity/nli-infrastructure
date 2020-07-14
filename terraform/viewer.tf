50021f8ab38fa1aee1f4863034428bf56b7f655c
Brian Helba <brian.helba@kitware.com>
2020-07-14 16:33:50 -0400

    Format TF

--- terraform/viewer.tf
+++ terraform/viewer.tf
@@ -1,8 +1,8 @@
 resource "aws_route53_record" "viewer" {
   zone_id = aws_route53_zone.domain.zone_id
-  name = "viewer"
-  type = "CNAME"
-  ttl = "300"
+  name    = "viewer"
+  type    = "CNAME"
+  ttl     = "300"
   records = [
     "flungui.netlify.com",
   ]
--- terraform/www.tf
+++ terraform/www.tf
@@ -1,8 +1,8 @@
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.domain.zone_id
-  name = "www"
-  type = "CNAME"
-  ttl = "300"
+  name    = "www"
+  type    = "CNAME"
+  ttl     = "300"
   records = [
     "nli-landing.netlify.com",
   ]
@@ -10,9 +10,9 @@ resource "aws_route53_record" "www" {
 
 resource "aws_route53_record" "www_apex" {
   zone_id = aws_route53_zone.domain.zone_id
-  name = "" # apex
-  type = "A"
-  ttl = "300"
+  name    = "" # apex
+  type    = "A"
+  ttl     = "300"
   records = [
     # https://docs.netlify.com/domains-https/custom-domains/configure-external-dns/#configure-an-apex-domain
     "104.198.14.52",
