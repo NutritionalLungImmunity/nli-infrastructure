6f6b9ba0edbd85ccbbbe1225ccc31128a7380e1d
Jonathan Beezley <jonathan.beezley@kitware.com>
2020-12-30 13:24:27 -0500

    Add a subdomain for the simulation worker

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -5,3 +5,13 @@ resource "aws_route53_zone" "domain" {
 resource "aws_route53_zone" "domain_alternate" {
   name = "nutritionallungimmunity.com"
 }
+
+resource "aws_route53_record" "worker" {
+  zone_id = aws_route53_zone.domain.zone_id
+  name    = "worker"
+  type    = "A"
+  ttl     = "300"
+  records = [
+    aws_eip.worker.public_ip
+  ]
+}
